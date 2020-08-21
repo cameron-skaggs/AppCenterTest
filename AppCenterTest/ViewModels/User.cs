f3ba48dfb102fe3d2c0c60668ecd2383ba1d7566
skaggs1995 <skaggs1995@gmail.com>
2020-08-21 19:18:00 -0400

    Added view-models to follow MVVM

---
+++ AppCenterTest/ViewModels/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCenterTest.ViewModels
+{
+    class User
+    {
+        private int _id;
+        public int Id { get; set; }
+
+        private string _name;
+        public string Name { get; set; }
+
+
+    }
+}
